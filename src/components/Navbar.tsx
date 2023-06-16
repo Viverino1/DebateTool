ca161a0ed050974e76a34e84c255f0c7e750debd
Verge Robotics <VergeRoboticsFTC@gmail.com>
2023-06-16 13:16:31 -0500

    bg

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,24 @@
-import RedButton from "./general/RedButton";
-
+import { ReactElement } from "react";
 export default function Navbar(){
     return(
         <div className="group flex flex-col space-y-4 w-20 hover:w-72 h-screen p-4 bg-neutral-800 overflow-clip
         transition-all duration-300">
-            <div className="w-full h-16">
-                
-            </div>
+            <NavbarElement icon={<img src="./DebateToolLogo.svg"/>} text="Debate Tool" to="/"/>
         </div>
     )
 }
 
-function NavbarElement(){
-    
+function NavbarElement(props: {icon: ReactElement, text: string, to: string}){
+    const {icon, text, to} = props;
+    return(
+        <div className="relative top-0 right-0 left-0 bottom-0 w-full h-12">
+            <div className="absolute z-0 top-0 right-0 left-0 bottom-0 w-full h-full rounded-lg bg-emerald-500 blur-md opacity-50"/>
+            <div className="absolute z-10 top-0 right-0 left-0 bottom-0 w-full h-full p-2 rounded-lg bg-emerald-500 overflow-clip">
+                <div className="flex items-center space-x-2 w-72 h-full">
+                    <div className="w-8 h-8">{icon}</div>
+                    <div className="text-lg">{text}</div>
+                </div>
+            </div>
+        </div>
+    )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        bg: {
+          '0%, 100%': { transform: 'translate(0vh)' },
+          '50%': { transform: 'translate(-50vh, 50vh)' },
+        }
+      },
+      animation: {
+        bg: 'pulse 30s ease-in-out infinite'
+      }
+    },
   },
   plugins: [],
 }
